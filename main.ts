553069c991d2e57832d139a8136fd2ff41aff9ce
I²C Programmierung <73078489+elssner@users.noreply.github.com>
2025-10-06 18:59:59 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,72 @@
+function mqtt_publish_joystick () {
+    pins.read_joystick()
+    if (joystick_fahren == pins.get_x() && joystick_lenken == pins.get_y()) {
+        pins.comment(pins.pins_text("wenn nichts geändert, kein Publish"))
+        return true
+    } else {
+        i_payload += 1
+        if (serial.mqtt_publish("topic", "" + i_payload + ";j;" + pins.get_x() + ";" + pins.get_y())) {
+            pins.comment(pins.pins_text("wenn Publish erfolgreich, x y Werte speichern"))
+            joystick_fahren = pins.get_x()
+            joystick_lenken = pins.get_y()
+            lcd.write_array(serial.get_response(), lcd.eINC.inc0, serial.get_response_index())
+            return true
+        } else {
+            pins.comment(pins.pins_text("wenn Publish false, nach Pause noch mal versuchen"))
+            lcd.write_array(serial.get_response(), lcd.eINC.inc0, serial.get_response_index())
+            basic.pause(200)
+            return false
+        }
+    }
+}
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    lcd.write_array(serial.get_response(), lcd.eINC.inc1)
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    basic.setLedColors(0x000000, 0x000000, 0x0000ff)
+    i_payload = 0
+    while (true) {
+        mqtt_publish_joystick()
+        basic.pause(100)
+    }
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.setLedColors(0x000000, 0xffffff, 0x000000)
+    if (serial.wifi_connect("TXT4.0-sWp6", "ozvTwHC7")) {
+        basic.setLedColors(0x000000, 0x00ff00, 0x000000)
+    } else {
+        basic.setLedColors(0x000000, 0xff0000, 0x000000)
+    }
+    lcd.write_array(serial.get_response(), lcd.eINC.inc0, serial.get_response_index())
+})
+input.onButtonEvent(Button.B, input.buttonEventValue(ButtonEvent.Hold), function () {
+    basic.setLedColors(0x000000, 0x000000, 0xffffff)
+    if (!(serial.mqtt_client("calliope"))) {
+        basic.setLedColors(0x000000, 0x000000, 0xff0000)
+    } else if (!(serial.mqtt_connect("192.168.8.2", 1884))) {
+        basic.setLedColors(0x000000, 0x000000, 0x00ffff)
+    } else {
+        basic.setLedColors(0x000000, 0x000000, 0x00ff00)
+    }
+    lcd.write_array(serial.get_response(), lcd.eINC.inc0, serial.get_response_index())
+})
+let i_payload = 0
+let joystick_lenken = 0
+let joystick_fahren = 0
+basic.setLedColors(0xffffff, 0x000000, 0x000000)
+serial.init_serial()
+basic.setLedColors(0xffffff, 0xffffff, 0x000000)
+basic.pause(2000)
+lcd.init_display(lcd.eDisplay.qwiic_20_4, true)
+if (lcd.get_display(lcd.eDisplay.none)) {
+    basic.setLedColors(0xffffff, 0xffffff, 0xff0000)
+    basic.pause(2000)
+} else {
+    basic.setLedColors(0xffffff, 0xffffff, 0xffffff)
+}
+if (serial.at_command(serial.serial_eAT(serial.eAT_commands.at_rst), 5)) {
+    basic.setLedColors(0x00ff00, 0x000000, 0x000000)
+} else {
+    basic.setLedColors(0xff0000, 0x000000, 0x000000)
+}
+lcd.write_array(serial.get_response(), lcd.eINC.inc0, serial.get_response_index(), 0)
